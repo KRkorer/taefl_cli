75e8718c8434737ec12dc79798e99907ac394cd6
Konstantin Korzhenkov <k.korzhenkov@outlook.com>
2022-04-06 09:33:37 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
+
+class Taefl {
+private:
+    /* data */
+
+public:
+    Taefl (arguments);
+
+};
+
+class Board {
+private:
+    char[9][9];
+
+public:
+    Board ();
+};
+
+class Piece {
+private:
+    /* data */
+
+public:
+    Piece ();
+};
+
+class Player {
+private:
+    /* data */
+
+public:
+    Player ();
+};
+
+class UI {
+private:
+    /* data */
+
+public:
+    UI ();
+
+};
