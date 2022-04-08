6fb133c6a9826ff45027129c9772ac59d689a5b9
Konstantin Korzhenkov <k.korzhenkov@outlook.com>
2022-04-08 19:26:06 +0300

    add loop

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 
 int main (int argc, char const *argv[]) {
     Taefl taefl;
-    taefl.game();
+    bool stop = false;
+    char answer = 'y';
+    while (!stop) {
+        taefl.game();
+        std::cout << "Would you like to play once more? (y/n)" << '\n';
+        std::cin >> answer;
+        if ((answer == 'n') or (answer == 'N')){
+            stop = true;
+        }
+    }
+
     return 0;
 }
